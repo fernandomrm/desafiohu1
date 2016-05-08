a44862931361a256888d616d493e616c5407c6f5
Fernando <fernandomarcio13@gmail.com>
2016-05-08 12:47:39 -0300

    Cria model de hotéis e disponibilidades

--- backend/desafiohu/models.py
+++ backend/desafiohu/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Hotel(models.Model):
+    nome = models.CharField(max_length=250)
+    cidade = models.CharField(max_length=250)
+
+
+class Disponibilidade(models.Model):
+    hotel = models.ForeignKey('Hotel')
+    data = models.DateField()
+    disponivel = models.BooleanField()
