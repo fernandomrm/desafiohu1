ff91956809220a1ba0425aff8903997ac9150a53
Fernando <fernandomarcio13@gmail.com>
2016-05-08 12:58:47 -0300

    Implementa criação da amostra de pesquisa

--- backend/desafiohu/models.py
+++ backend/desafiohu/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 
 
+class HotelManager(models.Manager):
+
+    def cria_amostra(self):
+        cidades_e_hoteis = self.values_list('cidade', 'nome')
+        cidades, hoteis = zip(*cidades_e_hoteis)
+        return list(set(cidades)) + list(set(hoteis))
+
+
 class Hotel(models.Model):
     nome = models.CharField(max_length=250)
     cidade = models.CharField(max_length=250)
+
+    objects = HotelManager()
 
 
 class Disponibilidade(models.Model):
--- backend/desafiohu/tests.py
+++ backend/desafiohu/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Hotel
+
+
+class BuscaHotelCidadeTestCase(TestCase):
+
+    def setUp(self):
+        hotel = Hotel.objects.create(cidade='Rio de Janeiro', nome='Hotel Urbano')
+
+    def test_cria_amostra_de_pesquisa_de_hoteis_e_disponibilidades(self):
+        amostra = Hotel.objects.cria_amostra()
+        self.assertEquals(len(amostra), 2)
