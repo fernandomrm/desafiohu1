7565bdcf07f7ba1436c26ec277a70e933fc7d88a
Fernando <fernandomarcio13@gmail.com>
2016-05-08 23:26:09 -0300

    Cria reducer de hotéis disponíveis

--- frontend/src/reducers.js
+++ frontend/src/reducers.js
@@ -1,9 +1,18 @@
-import { BUSCA_HOTEIS_SUCCESS } from './actions';
+import { BUSCA_HOTEIS_SUCCESS, BUSCA_HOTEIS_DISPONIVEIS_SUCCESS } from './actions';
 
 export function hoteis(state = [], action) {
     switch (action.type) {
         case BUSCA_HOTEIS_SUCCESS:
             return action.hoteis;
+        default:
+            return state
+    }
+}
+
+export function hoteisDisponiveis(state = [], action) {
+    switch (action.type) {
+        case BUSCA_HOTEIS_DISPONIVEIS_SUCCESS:
+            return action.hoteisDisponiveis;
         default:
             return state
     }
--- frontend/tests/reducers.js
+++ frontend/tests/reducers.js
@@ -1,6 +1,6 @@
 import expect from 'expect'
-import { hoteis } from '../src/reducers'
-import { BUSCA_HOTEIS_SUCCESS } from '../src/actions';
+import { hoteis, hoteisDisponiveis } from '../src/reducers'
+import { BUSCA_HOTEIS_SUCCESS, BUSCA_HOTEIS_DISPONIVEIS_SUCCESS } from '../src/actions';
 
 describe('Reducers', () => {
     it('Reducer de hotéis retorna uma lista vazia por padrão', () => {
@@ -13,6 +13,20 @@ describe('Reducers', () => {
             hoteis([], {
                 type: BUSCA_HOTEIS_SUCCESS,
                 hoteis: _hoteis
+            })
+        ).toEqual(_hoteis);
+    })
+
+    it('Reducer de hotéis disponíveis retorna uma lista vazia por padrão', () => {
+        expect(hoteisDisponiveis(undefined, {})).toEqual([]);
+    })
+
+    it('Reducer de hotéis disponíveis altera estado com a action BUSCA_HOTEIS_DISPONIVEIS_SUCCESS', () => {
+        var _hoteis = [{id: 1, nome: 'Hotel Urbano', cidade: 'Rio de Janeiro'}];
+        expect(
+            hoteisDisponiveis([], {
+                type: BUSCA_HOTEIS_DISPONIVEIS_SUCCESS,
+                hoteisDisponiveis: _hoteis
             })
         ).toEqual(_hoteis);
     })
