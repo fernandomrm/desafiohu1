b04a15574659fb10eda833f4c6c1494a98de9c37
Fernando <fernandomarcio13@gmail.com>
2016-05-08 23:00:10 -0300

    Cria action para a busca de hotéis disponíveis

--- frontend/src/actions.js
+++ frontend/src/actions.js
@@ -2,6 +2,8 @@ import { fetch } from  'whatwg-fetch';
 
 export const BUSCA_HOTEIS_REQUEST = 'BUSCA_HOTEIS_REQUEST';
 export const BUSCA_HOTEIS_SUCCESS = 'BUSCA_HOTEIS_SUCCESS';
+export const BUSCA_HOTEIS_DISPONIVEIS_REQUEST = 'BUSCA_HOTEIS_DISPONIVEIS_REQUEST';
+export const BUSCA_HOTEIS_DISPONIVEIS_SUCCESS = 'BUSCA_HOTEIS_DISPONIVEIS_SUCCESS';
 
 function buscaHoteisRequest() {
     return {
@@ -22,5 +24,28 @@ export function buscaHoteis(query) {
         return fetch('http://localhost:8000/busca-hoteis', {query: query})
             .then(res => res.json())
             .then(json => dispatch(buscaHoteisSuccess(json)))
+    }
+}
+
+function buscaHoteisDisponiveisRequest() {
+    return {
+        type: BUSCA_HOTEIS_DISPONIVEIS_REQUEST
+    }
+}
+
+function buscaHoteisDisponiveisSuccess(hoteisDisponiveis) {
+    return {
+        type: BUSCA_HOTEIS_DISPONIVEIS_SUCCESS,
+        hoteisDisponiveis
+    }
+}
+
+export function buscaHoteisDisponiveis(query, dataInicio, dataFim) {
+    return dispatch => {
+        let data = {query: query, data_inicio: dataInicio, data_fim: dataFim};
+        dispatch(buscaHoteisDisponiveisRequest());
+        return fetch('http://localhost:8000/busca-hoteis-disponiveis', data)
+            .then(res => res.json())
+            .then(json => dispatch(buscaHoteisDisponiveisSuccess(json)))
     }
 }
--- frontend/tests/actions.js
+++ frontend/tests/actions.js
@@ -3,7 +3,15 @@ import thunk from 'redux-thunk';
 import nock from 'nock';
 import expect from 'expect';
 
-import { buscaHoteis, BUSCA_HOTEIS_SUCCESS, BUSCA_HOTEIS_REQUEST } from '../src/actions';
+import {
+    buscaHoteis,
+    BUSCA_HOTEIS_SUCCESS,
+    BUSCA_HOTEIS_REQUEST,
+    buscaHoteisDisponiveis,
+    BUSCA_HOTEIS_DISPONIVEIS_SUCCESS,
+    BUSCA_HOTEIS_DISPONIVEIS_REQUEST
+ } from '../src/actions';
+
 
 const middlewares = [ thunk ];
 const mockStore = configureMockStore(middlewares);
@@ -27,6 +35,25 @@ describe('Actions', () => {
         const store = mockStore({ hoteis: [] });
 
         return store.dispatch(buscaHoteis('Hotel Urbano'))
+            .then(() => {
+                expect(store.getActions()).toEqual(expectedActions)
+            });
+    })
+
+    it('Cria action BUSCA_HOTEIS_DISPONIVEIS_SUCCESS ao concluir a busca de hóteis disponíveis com sucesso', () => {
+        var hoteis = [{id: 1, nome: 'Hotel Urbano', cidade: 'Rio de Janeiro'}];
+
+        nock('http://localhost:8000/')
+        .get('/busca-hoteis-disponiveis')
+        .reply(200, hoteis);
+
+        const expectedActions = [
+            { type: BUSCA_HOTEIS_DISPONIVEIS_REQUEST },
+            { type: BUSCA_HOTEIS_DISPONIVEIS_SUCCESS, hoteisDisponiveis: hoteis }
+        ];
+        const store = mockStore({ hoteisDisponiveis: [] });
+
+        return store.dispatch(buscaHoteisDisponiveis('Hotel Urbano', '3/1/2016', '5/1/2016'))
             .then(() => {
                 expect(store.getActions()).toEqual(expectedActions)
             });
