517c0c2c6445044616b1b2c9671a5a7e5ec3aeab
Fernando <fernandomarcio13@gmail.com>
2016-05-09 01:08:46 -0300

    Cria componente para a tabela de horários disponíveis

--- frontend/src/components.js
+++ frontend/src/components.js
@@ -11,3 +11,40 @@ export class Header extends Component {
         )
     }
 }
+
+
+export class TabelaHoteisDisponiveis extends Component {
+    renderHoteis() {
+        const { hoteisDisponiveis } = this.props;
+        if (hoteisDisponiveis.length) {
+            return hoteisDisponiveis.map((hotel, index) =>
+                <tr key={index}>
+                    <td>{hotel.nome}</td>
+                    <td>{hotel.cidade}</td>
+                </tr>
+            )
+        } else {
+            return <tr><td colSpan="2">Nenhum hotel encontrado</td></tr>;
+        }
+    }
+
+    render() {
+        return (
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Hotel</th>
+                        <th>Cidade</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    { this.renderHoteis() }
+                </tbody>
+            </table>
+        )
+    }
+}
+
+TabelaHoteisDisponiveis.propTypes = {
+    hoteisDisponiveis: PropTypes.array.isRequired
+}
--- frontend/tests/components.js
+++ frontend/tests/components.js
@@ -2,33 +2,63 @@ import expect from 'expect';
 import React from 'react';
 import TestUtils from 'react-addons-test-utils';
 
-import { Header } from '../src/components';
+import { Header, TabelaHoteisDisponiveis } from '../src/components';
 
 describe('Components', () => {
     describe('Header', () => {
 
         function setup() {
-            let renderer = TestUtils.createRenderer()
-            renderer.render(<Header />)
-            let output = renderer.getRenderOutput()
-
-            return {
-                output,
-                renderer
-            }
+            let renderer = TestUtils.createRenderer();
+            renderer.render(<Header />);
+            let output = renderer.getRenderOutput();
+            return output;
         }
 
         it('Renderiza header corretamente', () => {
-            const { output } = setup()
+            const output = setup();
 
-            expect(output.type).toBe('header')
+            expect(output.type).toBe('header');
 
-            let [ icone, h1 ] = output.props.children
+            let [ icone, h1 ] = output.props.children;
 
             expect(icone.type).toBe('i');
             expect(icone.props.className).toBe('icon-hoteis');
             expect(h1.type).toBe('h1');
             expect(h1.props.children).toBe('HOTÉIS');
+        })
+    })
+
+    describe('Tabela de hotéis disponíveis', () => {
+
+        function setup(hoteis) {
+            let renderer = TestUtils.createRenderer();
+            renderer.render(<TabelaHoteisDisponiveis hoteisDisponiveis={hoteis} />);
+            let output = renderer.getRenderOutput();
+            return output;
+        }
+
+        it('Renderiza tabela de hotéis disponíveis vazia', () => {
+            let hoteis = [];
+            const output = setup(hoteis);
+
+            expect(output.type).toBe('table');
+
+            let [ thead, tbody ] = output.props.children;
+            let td = tbody.props.children.props.children;
+
+            expect(td.props.children).toBe('Nenhum hotel encontrado');
+        })
+
+        it('Renderiza tabela de hotéis disponíveis com resultados', () => {
+            let hoteis = [{nome: 'Hotel Urbano', cidade: 'Rio de Janeiro'}, {nome: 'Outro Hotel', cidade: 'São Paulo'}];
+            const output = setup(hoteis);
+
+            expect(output.type).toBe('table');
+
+            let [ thead, tbody ] = output.props.children;
+            let rows = tbody.props.children;
+
+            expect(rows.length).toBe(2);
         })
     })
 })
