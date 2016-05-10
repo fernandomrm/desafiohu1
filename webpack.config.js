f6228935c6e54f1f5a14634d916be5fd0656c1ae
Fernando <fernandomarcio13@gmail.com>
2016-05-10 15:22:27 -0300

    Implementa build de css, scss, imagens e atualiza dependências

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports =
 {
     entry: './frontend/src',
@@ -14,7 +16,25 @@ module.exports =
                 query: {
                     presets: ['es2015', 'react']
                 },
+            },
+            {
+                test: /\.scss/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader')
+            },
+            {
+                test: /\.css/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loaders: [
+                    'file?hash=sha512&digest=hex&name=[hash].[ext]',
+                    'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+                ]
             }
         ],
-    }
+    },
+    plugins: [
+        new ExtractTextPlugin('style.css', {allChunks: true})
+    ]
 };
